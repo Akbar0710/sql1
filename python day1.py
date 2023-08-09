644c2cd19500e938a403089475a2013c3f9f301e
Akbar0710 <140975112+Akbar0710@users.noreply.github.com>
2023-08-09 20:04:06 +0530

    Add files via upload

---
+++ python day1.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name1 = "garry"
+name = "hena"
+print(name1)
+
+
+# In[3]:
+
+
+ravi_age = ram_age = yakub_age = 21
+print("ravi age =",ravi_age)
+print("ram age =",ram_age)
+print("yakub age =",yakub_age)
+
+
+# In[4]:
+
+
+age = 21
+print(age)
+type(age)
+
+
+# In[24]:
+
+
+price_of_coke = 20.9
+price_of_maggie = 20.6
+print(price_of_coke)
+print(price_of_maggie)
+type(price_of_coke)
+
+type(price_of_maggie)
+
+
+# In[13]:
+
+
+price_book = 30.6
+num_of_book = 6
+total = 0
+total = price_book * num_of_book
+print(total)
+type(total)
+
+
+# In[18]:
+
+
+price = 290
+tax = "2.5"
+total = price+float(tax)
+print(type(total))
+
+
+# # type casting
+
+# In[27]:
+
+
+marks = 50.6
+print(marks)
+str(marks)
+pas,fail = 1,0
+print(bool(pas))
+print(bool(fail))
+
+
+# In[28]:
+
+
+a = "2.5"
+int(a)
+
+
+# In[29]:
+
+
+print("price of coke"+20.5)
+
+
+# In[30]:
+
+
+print("ravi age is"+str(50))
+
+
+# # arithmetic
+
+# In[31]:
+
+
+price_book = 30.6
+num_of_book = 6
+total = 0
+total = price_book * num_of_book
+print(total)
+type(total)
+
+
+# In[32]:
+
+
+price_dress = 1500
+num_dress = 30
+total = price_dress * num_dress
+
+print(total)
+
+
+# In[33]:
+
+
+book1 = 20
+book2 = 30
+total_book = book1 + book2
+print(total_book)
+
+
+# In[34]:
+
+
+side = 20
+print("square side ",side**2)
+print("cube side ",side**3)
+
+
+# In[ ]:
+
+
+
+
